91b919c89fe6a78143b8d29e47ae7542ee2963bb
Jacob <jjq.quirky@gmail.com>
2025-04-14 19:58:01 -0700

    added script to manage ui

---
+++ scripts/ui.gd
@@ -0,0 +1,25 @@
+extends CanvasLayer
+
+@onready var inventory = $UI/Inventory
+
+func get_active_ui():
+	var ui = $UI
+	for i in range(len(ui.get_children())):
+		var control = ui.get_child(i) as Control
+		if control:
+			if control.name != "Menu" and control.visible:
+				return control
+	return null
+
+func close_active_ui():
+	get_tree().paused = false
+	var control = get_active_ui()
+	if control:
+		control.visible = false
+
+func _on_close_pressed():
+	close_active_ui()
+
+func _on_inventory_pressed():
+	get_tree().paused = true
+	inventory.visible = true
