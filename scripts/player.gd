732c0280c4006f69eaf02763c9b80459f2a83169
Jacob <jjq.quirky@gmail.com>
2025-04-26 15:27:47 -0700

    addeding enemy

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,86 +1,20 @@
 extends CharacterBody2D
 class_name Test_Player
 @onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
-#@onready var tile_map = $"../Pathing"
-
-#
-#var astar_grid: AStarGrid2D
-#var current_id_path: Array[Vector2i]
-#var current_point_path: PackedVector2Array
-#var target_position: Vector2
-#var is_moving: bool
-#
-#
-#func _ready() -> void:
-	#astar_grid = AStarGrid2D.new()
-	#astar_grid.region = tile_map.get_used_rect()
-	#astar_grid.cell_size = Vector2(16,16)
-	#astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_ALWAYS
-	#astar_grid.update()
-	#
-	#for x in tile_map.get_used_rect().size.x:
-		#for y in tile_map.get_used_rect().size.y:
-			#var tile_position = Vector2i(
-				#x + tile_map.get_used_rect().position.x,
-				#y + tile_map.get_used_rect().position.y
-			#)
-			#
-			#var tile_data = tile_map.get_cell_tile_data(tile_position)
-			#
-			#if tile_data == null or tile_data.get_custom_data("walkable") == false:
-				#astar_grid.set_point_solid(tile_position)
-	#
-	#
-#func _input(event):
-	#if event.is_action_pressed("move") == false:
-		#return
-		#
-	#var id_path
-	#
-	#if is_moving:
-		#id_path = astar_grid.get_id_path(
-			#tile_map.local_to_map(target_position),
-			#tile_map.local_to_map(get_global_mouse_position())
-		#).slice(1)
-	#else:
-		#id_path = astar_grid.get_id_path(
-			#tile_map.local_to_map(global_position),
-			#tile_map.local_to_map(get_global_mouse_position())
-		#).slice(1)
-		#
-	#if id_path.is_empty() == false:
-		#current_id_path = id_path
-		#
-	#current_point_path = astar_grid.get_point_path(
-		#tile_map.local_to_map(target_position),
-		#tile_map.local_to_map(get_global_mouse_position())
-	#)
-	#
-	#for i in current_point_path.size():
-		#current_point_path[i] = current_point_path[i] + Vector2(8,8)
-#
-#func _physics_process(delta: float) -> void:
-	#if current_id_path.is_empty():
-		#return
-	#
-	#if is_moving == false:
-		#target_position = tile_map.map_to_local(current_id_path.front())
-		#is_moving = true
-	#
-	#global_position = global_position.move_toward(target_position, 5)
-	#
-	#if global_position == target_position:
-		#current_id_path.pop_front()
-		#
-		#if current_id_path.is_empty() == false:
-			#target_position = tile_map.map_to_local(current_id_path.front())
-		#else:
-			#is_moving = false
-
 
 var movement_speed = 200.0
 
+var touching_character = false
+
 func move_to(coordinates):
+	snap_pos()
+	var query = PhysicsPointQueryParameters2D.new()
+	query.position = coordinates
+	query.collide_with_bodies = true
+	var results = get_world_2d().direct_space_state.intersect_point(query)
+	for hit in results:
+		if hit and hit.collider is CharacterBody2D:
+			return
 	navigation_agent_2d.target_position = coordinates
 
 func _physics_process(delta: float) -> void:
@@ -102,6 +36,24 @@ func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
 func _on_pathing_move_to(coordinate):
 	move_to(coordinate)
 
+func snap_pos():
+	global_position = global_position.snapped(Vector2(16, 16))
+
 # snaps the position to the nearest cell on destination reached
 func _on_navigation_agent_2d_target_reached():
-	global_position = global_position.snapped(Vector2(16, 16))
+	snap_pos()
+
+func _on_area_2d_body_entered(body):
+	if body is CharacterBody2D:
+		# we need to stop moving to that square!
+		touching_character = true
+		snap_pos()
+
+func _on_area_2d_body_exited(body):
+	if body is CharacterBody2D:
+		# we need to stop moving to that square!
+		touching_character = false
+
+func _on_navigation_agent_2d_waypoint_reached(details):
+	if touching_character:
+		move_to(details.position)
